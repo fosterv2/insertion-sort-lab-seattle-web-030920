00a458027b271e3dd5fb353e1c93694d4a5e7fef
fosterv2 <fosterv2@outlook.com>
2020-08-12 14:48:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    let minInd = 0
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+            minInd = i
+        }
+    }
+    return array.splice(minInd, 1)[0]
 }
 
 function selectionSort(array){
-
+    const sorted = []
+    while (array.length > 0) {
+        sorted.push(findMinAndRemove(array))
+    }
+    return sorted
 }
